10d2e367e594d699a7ab7ccc30e61fa17b0f86ec
Afonso Jorge Ramos <up201506239@fe.up.pt>
2019-07-05 12:28:18 +0100

    baseline - working maps API

---
+++ index.js
@@ -0,0 +1,65 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyA_ioaEbPgfOjBB2DWiW-P4RJcCBEpaPP8',
+    Promise: Promise
+});
+//const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
+    }
+
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
+
+    const getNearbyMuseums = (city) => {
+        console.log("> getNearbyMuseums City: " + city);
+    
+        return googleMapsClient.places({ query: `${city}`, type: 'museum' }).asPromise()
+            .then((response) => {
+                console.log(response.json.results[0]);
+                return Promise.resolve(response.json.results[0].name);
+            })
+            .catch((err) => {
+                console.log(Promise.reject(err));
+            });
+    };
+
+    function activitiesHandler(agent) {
+        const params = agent.getContext('booking-context').parameters;
+        const city = params['geo-city'];
+        //agent.add('My name is Dialogflow!' + city);
+
+        return getNearbyMuseums(city)
+            .then((response) => {
+                agent.add(response);
+                return Promise.resolve(response);
+            })
+            .catch((err) => {
+                agent.add("Uh oh, something is wrong *bleeds*");
+                return Promise.resolve(err);
+            });
+    }
+
+    // Run the proper function handler based on the matched Dialogflow intent name
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('Activities', activitiesHandler);
+    // intentMap.set('your intent name here', googleAssistantHandler);
+    agent.handleRequest(intentMap);
+});
