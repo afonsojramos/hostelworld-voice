cb5e48b819051b8ce270c43a88020f07361f7180
Afonso Jorge Ramos <up201506239@fe.up.pt>
2019-07-08 11:39:03 +0100

    working photos

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 
 const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
+const placesAPIkey = `AIzaSyA_ioaEbPgfOjBB2DWiW-P4RJcCBEpaPP8`
 const googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyA_ioaEbPgfOjBB2DWiW-P4RJcCBEpaPP8',
+    key: placesAPIkey,
     Promise: Promise
 });
 const {
@@ -43,7 +44,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         return googleMapsClient.places({ query: `${city}`, type: 'museum' }).asPromise()
             .then((response) => {
                 console.log(response.json.results);
-                return Promise.resolve(response.json.results.slice(0, 3));
+                return Promise.resolve(response.json.results.slice(0, 4));
             })
             .catch((err) => {
                 console.log(Promise.reject(err));
@@ -69,19 +70,19 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                 conv.ask(new Carousel({
                     title: 'Google Assistant',
                     items: {
-                        'WorksWithGoogleAssistantItemKey': {
+                        'OptionOne': {
                             title: `${response[0].name}`,
                             description: `${response[0].rating} stars!`,
                             image: {
-                                url: response[0].icon,
+                                url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=${response[0].photos[0].photo_reference}&key=${placesAPIkey}`,
                                 accessibilityText: `${response[0].name}`,
                             },
                         },
-                        'GoogleHomeItemKey': {
+                        'OptionTwo': {
                             title: `${response[1].name}`,
                             description: `${response[1].rating} stars!`,
                             image: {
-                                url: response[1].icon,
+                                url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=${response[1].photos[0].photo_reference}&key=${placesAPIkey}`,
                                 accessibilityText: `${response[1].name}`
                             },
                         },
