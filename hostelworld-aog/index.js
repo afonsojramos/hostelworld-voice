650f09e683f5ee55c5c196ba4d7d521fa2385798
Afonso Jorge Ramos <up201506239@fe.up.pt>
2019-07-18 12:26:46 +0100

    added support for no-screen devices

--- hostelworld-aog/index.js
+++ hostelworld-aog/index.js
@@ -21,6 +21,8 @@ app.intent('Hostels - Permission Confirmed', (conv, params, confirmationGranted)
     const { location } = conv.device;
     const { date, geo_city, map_sort, hostel_type } = conv.contexts.input[`booking-context`].parameters;
 
+    console.log(location);
+
     if (confirmationGranted && name && location) {
         conv.ask(`Hello ${name.given}! Nice to meet you! ` +
             `I hope I can find something for you either in ${location.city} or anywhere else in the world!`);
@@ -41,11 +43,6 @@ app.intent('Hostels - Permission Confirmed', (conv, params, confirmationGranted)
                         }
                     }
 
-                    if (!conv.screen) {
-                        conv.ask('Sorry, try this on a screen device or select the phone surface in the simulator.');
-                        return;
-                    }
-
                     conv.ask(`These are my recomendations for ${city.name}!`);
 
                     return getHostels(city)
@@ -53,7 +50,7 @@ app.intent('Hostels - Permission Confirmed', (conv, params, confirmationGranted)
                             const parsedProperties = JSON.parse(propertiesResponse).properties;
                             console.log(parsedProperties);
 
-                            conv.ask(createPropertiesCarousel(city, parsedProperties));
+                            conv.ask(createPropertiesCarousel(city, parsedProperties, conv.screen));
                         })
                         .catch((err) => {
                             conv.ask("Uh oh, something is wrong TWO *bleeds*" + err);
@@ -79,16 +76,26 @@ app.intent('Hostels - Permission Confirmed', (conv, params, confirmationGranted)
 
 app.intent('Hostels', (conv, { date, geo_city, map_sort, hostel_type }, confirmationGranted) => {
 
-    console.log(conv.user.permissions.includes('DEVICE_PRECISE_LOCATION'));
+    if (geo_city === '' && !conv.user.permissions.includes('DEVICE_PRECISE_LOCATION') && !conv.user.permissions.includes('DEVICE_COARSE_LOCATION')) {
 
-    if (geo_city === '' && !conv.user.permissions.includes('DEVICE_PRECISE_LOCATION')) {
-        const options = {
-            context: 'To address you by name and know your location',
-            permissions: ['NAME', 'DEVICE_PRECISE_LOCATION'],
-        };
+        var options;
+
+        if (conv.screen) {
+            options = {
+                context: 'To address you by name and know your location',
+                permissions: ['NAME', 'DEVICE_PRECISE_LOCATION'],
+            };
+        } else {
+            options = {
+                context: 'To address you by name and know your location',
+                permissions: ['NAME', 'DEVICE_COARSE_LOCATION'],
+            };
+        }
         conv.ask(new Permission(options));
+
         return;
-    } else if (geo_city === '' && conv.user.permissions.includes('DEVICE_PRECISE_LOCATION')) {
+
+    } else if (geo_city === '' && (conv.user.permissions.includes('DEVICE_PRECISE_LOCATION') || conv.user.permissions.includes('DEVICE_COARSE_LOCATION'))) {
         const { location } = conv.device;
         geo_city = location.city;
     }
@@ -106,11 +113,6 @@ app.intent('Hostels', (conv, { date, geo_city, map_sort, hostel_type }, confirma
                 }
             }
 
-            if (!conv.screen) {
-                conv.ask('Sorry, try this on a screen device or select the phone surface in the simulator.');
-                return;
-            }
-
             conv.ask(`These are my recomendations for ${city.name}!`);
 
             return getHostels(city, map_sort, hostel_type)
@@ -118,7 +120,7 @@ app.intent('Hostels', (conv, { date, geo_city, map_sort, hostel_type }, confirma
                     const parsedProperties = JSON.parse(propertiesResponse).properties;
                     console.log(parsedProperties);
 
-                    conv.ask(createPropertiesCarousel(city, parsedProperties));
+                    conv.ask(createPropertiesCarousel(city, parsedProperties, conv.screen));
                 })
                 .catch((err) => {
                     conv.ask("Uh oh, something is wrong TWO *bleeds*" + err);
@@ -161,45 +163,50 @@ const getCityId = (city) => {
     }
 };
 
-const createPropertiesCarousel = (city, parsedProperties) => {
-    return new Carousel({
-        title: `${city.name}'s Top 4`,
-        items: {
-            'OptionOne': {
-                title: `${parsedProperties[0].name}`,
-                description: `${parsedProperties[0].overallRating.overall / 10}/10!`,
-                image: {
-                    url: `https://${parsedProperties[0].images[0].prefix + parsedProperties[0].images[0].suffix}`,
-                    accessibilityText: `${parsedProperties[0].name}`,
+const createPropertiesCarousel = (city, parsedProperties, screen) => {
+    if (screen) {
+        return new Carousel({
+            title: `${city.name}'s Top 4`,
+            items: {
+                'OptionOne': {
+                    title: `${parsedProperties[0].name}`,
+                    description: `${parsedProperties[0].overallRating.overall / 10}/10!`,
+                    image: {
+                        url: `https://${parsedProperties[0].images[0].prefix + parsedProperties[0].images[0].suffix}`,
+                        accessibilityText: `${parsedProperties[0].name}`,
+                    },
+                },
+                'OptionTwo': {
+                    title: `${parsedProperties[1].name}`,
+                    description: `${parsedProperties[1].overallRating.overall / 10}/10!`,
+                    image: {
+                        url: `https://${parsedProperties[1].images[0].prefix + parsedProperties[1].images[0].suffix}`,
+                        accessibilityText: `${parsedProperties[1].name}`
+                    },
+                },
+                'OptionThree': {
+                    title: `${parsedProperties[2].name}`,
+                    description: `${parsedProperties[2].overallRating.overall / 10}/10!`,
+                    image: {
+                        url: `https://${parsedProperties[2].images[0].prefix + parsedProperties[2].images[0].suffix}`,
+                        accessibilityText: `${parsedProperties[2].name}`
+                    },
+                },
+                'OptionFour': {
+                    title: `${parsedProperties[3].name}`,
+                    description: `${parsedProperties[3].overallRating.overall / 10}/10!`,
+                    image: {
+                        url: `https://${parsedProperties[3].images[0].prefix + parsedProperties[3].images[0].suffix}`,
+                        accessibilityText: `${parsedProperties[3].name}`
+                    },
                 },
             },
-            'OptionTwo': {
-                title: `${parsedProperties[1].name}`,
-                description: `${parsedProperties[1].overallRating.overall / 10}/10!`,
-                image: {
-                    url: `https://${parsedProperties[1].images[0].prefix + parsedProperties[1].images[0].suffix}`,
-                    accessibilityText: `${parsedProperties[1].name}`
-                },
-            },
-            'OptionThree': {
-                title: `${parsedProperties[2].name}`,
-                description: `${parsedProperties[2].overallRating.overall / 10}/10!`,
-                image: {
-                    url: `https://${parsedProperties[2].images[0].prefix + parsedProperties[2].images[0].suffix}`,
-                    accessibilityText: `${parsedProperties[2].name}`
-                },
-            },
-            'OptionFour': {
-                title: `${parsedProperties[3].name}`,
-                description: `${parsedProperties[3].overallRating.overall / 10}/10!`,
-                image: {
-                    url: `https://${parsedProperties[3].images[0].prefix + parsedProperties[3].images[0].suffix}`,
-                    accessibilityText: `${parsedProperties[3].name}`
-                },
-            },
-        },
-    });
-}
+        });
+    }
+    else {
+        return `${city.name}'s Top 4 hostels are comprised of ${parsedProperties[0].name}, ${parsedProperties[1].name}, ${parsedProperties[2].name} and ${parsedProperties[3].name}. Which one are you interested in?`;
+    }
+};
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
